23e63c398423a4b64bc473725acf019e4bb11d63
Samuel <samucarcoverde@gmail.com>
2024-10-11 16:45:40 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Veiculo {
+  modelo: string;
+  placa: string;
+  ano: number;
+  cor: string;
+  quilometragem: number;
+  combustivel: string;
+}
+
+const API_BASE_URL = 'http://localhost:8000/api';
+
+const getVeiculos = async () => {
+  try {
+    const response = await axios.get<Veiculo[]>(`${API_BASE_URL}/modelo/City`); // Tipagem explícita da resposta
+    console.log('Dados retornados pela API:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao obter veículos:', error);
+    throw error;
+  }
+};
+
+const HomePage = () => {
+  const [veiculos, setVeiculos] = useState<Veiculo[]>([]); // Tipagem do estado
+
+  useEffect(() => {
+    const fetchVeiculos = async () => {
+      try {
+        const veiculosData = await getVeiculos(); // Aqui a função já retorna o array tipado
+        setVeiculos(veiculosData);
+      } catch (error) {
+        console.error('Erro ao obter veículos:', error);
+      }
+    };
+
+    fetchVeiculos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Veículos</h1>
+      <ul>
+        {veiculos.map((veiculo) => (
+          <li key={veiculo.placa}>
+            {veiculo.modelo} - 
+            {veiculo.placa} - 
+            {veiculo.ano} - 
+            {veiculo.cor} -
+            {veiculo.quilometragem} KM - 
+            {veiculo.combustivel}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
