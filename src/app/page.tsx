2de63fcd763c778084a1eeda79419aecba6edf2a
Samuel <samucarcoverde@gmail.com>
2024-10-20 23:36:10 -0300

    start build front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import classes from './page.module.css'
 
 interface Veiculo {
   modelo: string;
@@ -44,8 +45,16 @@ const HomePage = () => {
   }, []);
 
   return (
+    <>
     <div>
-      <h1>Lista de Veículos</h1>
+    <header className={classes.header}>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Taste & share food from all over the world.</p>
+        </div>
+      </div>
+    </header>
       <ul>
         {veiculos.map((veiculo) => (
           <li key={veiculo.placa}>
@@ -59,6 +68,7 @@ const HomePage = () => {
         ))}
       </ul>
     </div>
+    </>
   );
 };
 
